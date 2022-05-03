43273d944edcf5ca719500347a7deeaf20db8072
Em <ekramm@me.com>
2022-05-03 18:12:17 -0400

    commit tree bfc65102b1c040e8582e89c504f3b93b888032f5

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
+  //include a fetch request to GOT API
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json()) //returned response is converted in JSON
+  .then((json) =>    renderBooks(json)); //prints new JSON to console and WE CHANGE THIS TO APPEND TO DOM??
+  
+
+  //then it should call the second function, renderBooks and pass in the JSON-ified data as the argument
+  //return fetch() in order for the test to pass
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
